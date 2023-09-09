072070ac6c57f7013ef5bed0986bdd85a25968d4
VedaDesai <143212418+VedaDesai@users.noreply.github.com>
2023-09-09 23:57:01 +0530

    Add files via upload

---
+++ code13.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+ int a,b,c;
+
+ printf("Enter 1st No: ");
+ scanf("%d",&a);
+  printf("Enter 2nd No: ");
+ scanf("%d",&b);
+ c=a+b;
+ printf("sum:%d",c);
+   return 0;
+}
